a069ddee7a31440c7ab107573eb5de16e1126c61
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-02-16 00:39:58 +0530

    [Ashish] Add:Binary Search Added

---
+++ Algorithms/BinarySearch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class BinarySearch
+    {
+        public int Search(string []list,string find, int start,int end)
+        {
+            if (start <= end)
+            {
+                int mid = (start + end) / 2;
+                if (list[mid].CompareTo(find)==0) return mid;
+              //  if (mid == 0) return false;
+                if (find.CompareTo(list[mid]) > 0) return Search(list, find, mid + 1, end);
+                else
+                    return Search(list, find, start, mid - 1);
+            }
+            else
+                return -1;
+        }
+    }
+}
