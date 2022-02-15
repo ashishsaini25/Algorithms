f3cb9317838059e50fef3e91f5a4b1be23981880
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-02-16 00:50:53 +0530

    [Ashish] Add:Sorted The String using insertion sort

---
+++ Algorithms/InsertionSort.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class InsertionSort
+    {
+        public void Sort(ref string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i].CompareTo(array[j]) > 0)
+                    {
+                        string temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -11,32 +11,11 @@ namespace Algorithms
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the List of Words Seperated By ','");
-            BinarySearch binarySearch = new BinarySearch();
             string given=Console.ReadLine();    
             string [] list = given.Split(',');
-            List<string> list2 = new List<string>();
-            foreach(string item in list)
-            {
-                list2.Add(item);
-            }
-            list2.Sort();
-            int i = 0;
-            foreach(string item in list2)
-            {
-                list[i] = item;
-                i++;
-            }
-            Console.WriteLine("Enter the string to search");
-            string find=Console.ReadLine();
-            int position = binarySearch.Search(list, find, 0, list.Length - 1);
-            if (position!=-1)
-            {
-                Console.WriteLine("Element is present at index: "+position);
-            }
-            else
-            {
-                Console.WriteLine("Element is not present");
-            }
+            InsertionSort insertionSort = new InsertionSort();
+            insertionSort.Sort(ref list);
+            for (int i = 0; i < list.Length; i++) Console.Write(list[i] + " ");
         }
     }
 }
