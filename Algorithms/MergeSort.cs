b29a15110d9aec69ca13a3036e720ad8da0bbee8
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-02-16 07:46:56 +0530

    [Ashish] Add:Merge Sort Added

---
+++ Algorithms/MergeSort.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class MergeSort
+    {
+        public List<string> Mergesort(List<string> unsorted)
+        {
+            if (unsorted.Count <= 1)
+                return unsorted;
+
+            List<string> left = new List<string>();
+            List<string> right = new List<string>();
+
+            int middle = unsorted.Count / 2;
+            for (int i = 0; i < middle; i++)  
+            {
+                left.Add(unsorted[i]);
+            }
+            for (int i = middle; i < unsorted.Count; i++)
+            {
+                right.Add(unsorted[i]);
+            }
+
+            left = Mergesort(left);
+            right = Mergesort(right);
+            return Merge(left, right);
+        }
+
+        public List<string> Merge(List<string> left, List<string> right)
+        {
+            List<string> result = new List<string>();
+
+            while (left.Count > 0 || right.Count > 0)
+            {
+                if (left.Count > 0 && right.Count > 0)
+                {
+                    if (left.First().CompareTo( right.First())<=0)  
+                    {
+                        result.Add(left.First());
+                        left.Remove(left.First());      
+                    }
+                    else
+                    {
+                        result.Add(right.First());
+                        right.Remove(right.First());
+                    }
+                }
+                else if (left.Count > 0)
+                {
+                    result.Add(left.First());
+                    left.Remove(left.First());
+                }
+                else if (right.Count > 0)
+                {
+                    result.Add(right.First());
+
+                    right.Remove(right.First());
+                }
+            }
+            return result;
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -13,9 +13,17 @@ namespace Algorithms
             Console.WriteLine("Enter the List of Words Seperated By ','");
             string given=Console.ReadLine();    
             string [] list = given.Split(',');
-           BubbleSort bubbleSort = new BubbleSort();
-            bubbleSort.Sort(ref list);
-            for (int i = 0; i < list.Length; i++) Console.Write(list[i] + " ");
+            List<string>list1=new List<string>();
+            List<string> list2= new List<string>();
+            for (int i=0;i<list.Length;i++) 
+                list1.Add(list[i]);
+            MergeSort mergeSort=new MergeSort();
+           list2= mergeSort.Mergesort(list1);
+            foreach(string s in list2)
+            {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
